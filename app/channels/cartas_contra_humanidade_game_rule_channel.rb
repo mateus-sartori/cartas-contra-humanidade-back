4ce4a33d612db4932589d1d8b8e34de213173b55
Mateus <mateus-sartori@hotmail.com>
2022-11-13 02:32:04 -0400

    feat: load white and black cards

--- app/channels/cartas_contra_humanidade_game_rule_channel.rb
+++ app/channels/cartas_contra_humanidade_game_rule_channel.rb
@@ -1,4 +1,6 @@
 class CartasContraHumanidadeGameRuleChannel < ApplicationCable::Channel
+  require 'json'
+
   def subscribed
     @session = params[:session]
     stream_for @session
@@ -14,39 +16,41 @@ class CartasContraHumanidadeGameRuleChannel < ApplicationCable::Channel
                                                        { data: data['bossRound'], action: 'set_boss_round' }
   end
 
+  def load_white_cards
+    data = JSON.parse(File.read('app/channels/white_cards.json'))
+    CartasContraHumanidadeGameRuleChannel.broadcast_to @session, { data:, action: 'load_white_cards' }
+  end
+
   def load_black_cards
-    data = [
-      {
-        id: 1,
-        text: 'Durante o sexo, gosto de pensar em _______'
-      },
-      {
-        id: 2,
-        text: 'Em seus momentos finais Michael Jackson pensou em _______'
-      },
-      {
-        id: 3,
-        text: 'Idosos tem cheiro de que?'
-      },
-      {
-        id: 4,
-        text: 'Guerra! Para que serve?'
-      },
-      {
-        id: 5,
-        text: 'O que me causa gases incontroláveis?'
-      },
-      {
-        id: 6,
-        text: 'Bebo para esquecer____'
-      },
-      {
-        id: 7,
-        text: 'Buscando aumentar sua audiência, o museu de História Natural abriu uma exposição interativa sobre ____'
-      }
-    ]
+    data = JSON.parse(File.read('app/channels/black_cards.json'))
 
     CartasContraHumanidadeGameRuleChannel.broadcast_to @session, { data:, action: 'load_black_cards' }
+  end
+
+  def load_cards_in_hands(data)
+    players = data['players']
+
+    info_players = []
+    white_cards = JSON.parse(File.read('app/channels/white_cards.json'))
+
+    players.each do |player|
+      random_cards = white_cards.sample(10)
+      white_cards -= random_cards
+
+      info_player = {
+        player:,
+        cards_in_hands: random_cards
+      }
+
+      info_players << info_player
+    end
+
+    info_data = {
+      info_players:,
+      white_cards:
+    }
+
+    CartasContraHumanidadeGameRuleChannel.broadcast_to @session, { data: info_data, action: 'load_cards_in_hands' }
   end
 
   def play_black_card(data)
